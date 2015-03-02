48ef8070b6dcdfe96deb022c18dc0ab9678a5818
harlo <harlo.holmes@gmail.com>
2015-03-02 18:18:05 -0500

    update fix

--- src/update.sh
+++ src/update.sh
@@ -4,9 +4,14 @@ source ~/.bash_profile
 
 function do_update {
 	cd $1
+	if ([ $# -eq 1 ]); then
+		G_BRANCH=master
+	else
+		G_BRANCH=$2
+
 	git fetch --all
-	git reset --hard origin/master
-	git pull origin master
+	git reset --hard origin/$G_BRANCH
+	git pull origin $G_BRANCH
 }
 
 C_A=~/InformaAnnex
@@ -14,5 +19,5 @@ C_F=~/InformaFrontend
 
 declare -a U_MODS=("$C_A" "$C_A/lib/Annex" "$C_A/lib/Annex/lib/Core" "$C_F" "$C_F/lib/Frontend" "$C_F/lib/Frontend/lib/Core")
 for U_MOD in "${U_MODS[@]}"; do
-	do_update $U_MOD
+	do_update $U_MOD $1
 done
